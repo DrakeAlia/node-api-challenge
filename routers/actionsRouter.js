b5e2b7d766b74ddbd6b5cf077d312b32c12151a9
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-06-05 12:03:56 -0700

    added middleware

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const Actions = require('../data/helpers/actiontModel.js');
+const Actions = require('../data/helpers/actionModel.js');
 
 const router = express.Router();
 
@@ -14,79 +14,73 @@ const router = express.Router();
 
 // custom middleware
 
-// Gets
 router.get('/', (req, res) => {
 	Actions.get()
-		.then((action) => {
-			res.status(200).json(action);
+		.then((actions) => {
+			res.status(200).json(actions);
 		})
-		.catch((err) => {
-			res.status(500).json({ error: 'Sorry, try again!', err });
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'Error retrieving the actions' });
 		});
 });
 
-router.get('/:id', validateActionId, (req, res) => {
-	const { id } = req.params;
-
-	Actions.get(id).then((action) => {
-		res.status(200).json(action);
-	});
-});
-
-// Puts
-router.put('/:id', validateActionId, validateAction, (req, res) => {
-	const { id } = req.params;
-
-	Actions.update(id, req.body).then((action) => {
-		res.status(200).json({ success: 'Info Updated!', info: req.body });
-	});
-});
-
-// Post
-router.post('/', validateAction, (req, res) => {
-	Actions.insert(req.body)
-		.then((action) => {
-			res.status(201).json({ success: 'A New Action was created!', action });
+router.get('/:id', validateId, (req, res) => {
+	Actions.get(req.actions)
+		.then((actions) => {
+			res.status(200).json(actions);
 		})
-		.catch((err) => {
-			res.status(500).json({ error: 'Sorry, try again!', err });
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'Error retrieving the actions' });
 		});
 });
 
-// Delete
-router.delete('/:id', validateActionId, (req, res) => {
-	const { id } = req.params;
-
-	Actions.get(id).then((action) => {
-		action
-			? Actions.remove(id).then((deleted) => {
-					deleted ? res.status(200).json({ success: `Project ${id} was deleted!`, info: action }) : null;
-				})
-			: null;
-	});
+router.put('/:id', validateActions, validateId, (req, res) => {
+	Actions.update(req.params.id, req.body)
+		.then((actions) => {
+				res.status(200).json(actions);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'Error updating the action' });
+		});
 });
 
-// middleware
-
-const validateActionId = (req, res, next) => {
-	const { id } = req.params;
-	Actions.get(id)
-		.then((action) => {
-			action ? req.action : res.status(404).json({ message: 'Action Does Not Exist!' });
+router.delete('/:id', validateId, (req, res) => {
+	Actions.remove(req.params.id)
+		.then((count) => {
+				res.status(200).json({ message: 'The action has been deleted' });
 		})
-		.catch((err) => {
-			res.status(500).json({ error: 'Sorry, try again!', err });
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'Error removing the action' });
 		});
-	next();
-};
+});
 
-const validateAction = (req, res, next) => {
-	const { description, notes } = req.body;
-	Object.entries(req.body).length === 0
-		? res.status(404).json({ message: 'No Action Data' })
-		: !description || !notes
-			? res.status(400).json({ message: 'Missing required info. Please add the description and notes!' })
-			: next();
-};
+//custom middleware
+
+function validateId(req, res, next) {
+    Actions.get(req.params.id)
+      .then(actions => {
+        if (!actions) {
+          res.status(404).json({ message: "actions id is invaild"});
+        } else {
+           req.actions = req.params.id;
+           next();
+        }
+      })
+  }
+  
+  function validateActions(req, res, next) {
+    if (Object.keys(req.body).length === 0) {
+      res.status(400).json({ message: 'actions data is missing'})
+    } else if (!req.body.notes || !req.body.description) {
+      res.status(400).json({ message: 'notes and description are required'})
+    } else {
+      return next()
+      }
+    }
+
 
 module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const Projects = require('./data/helpers/projectModel.js');
+const Projects = require('../data/helpers/projectModel.js');
 
 const router = express.Router();
 
@@ -19,110 +19,121 @@ const router = express.Router();
 // custom middleware
 
 router.get('/', (req, res) => {
-    Projects
-      .get()
-      .then(projects => {
-        res.status(200).json(projects);
-      })
-      .catch(error => {
-        res.status(500).json({ message: "Error retrieving the projects" });
-      }); 
-  });
-  
-  router.get('/:id', validateProjectId, (req, res) => {
-    const { id } = req.params;
-  
-    Projects.get(id)
-      .then(project => {
-      res.status(200).json(project);
-    });
-  });
-  
-  router.post("/", validateProject, (req, res) => {
-    Projects.insert(req.body)
-      .then(project => {
-        res.status(201).json({ success: 'A New Project was created!', project })
-      })
-      .catch(err => {
-        res.status(500).json({ error: 'Sorry, try again!', err })
-      })
-  });
-  
-  router.put("/:id", validateProjectId, validateProject, (req, res) => {
-    const { id } = req.params;
-  
-    Projects.update(id, req.body).then(project => {
-      res.status(200).json({ success: "Info Updated!", info: req.body });
-    });
-  });
-  
-  router.delete("/:id", validateProjectId, (req, res) => {
-    const { id } = req.params;
-  
-    Projects
-      .get(id)
-      .then(project => {
-        project
-          ? Projects
-            .remove(id)
-            .then(deleted => {
-              deleted
-                ? res.status(200).json({ success: `The Project ${id} was deleted!`, info: project }) : null
-            })
-          : null
-      }); 
-  });
-  
-  router.get("/:id/actions", validateProjectId, (req, res) => {
-    const { id } = req.params;
-  
-    Projects.getProjectActions(id)
-      .then(data => {
-        data ? res.status(200).json(data) : null
-      }) 
-  });
-  
-  router.post("/:id/actions", validateProjectId,  (req, res) => {
-    const { description, notes } = req.body;
-    const project_id = req.params.id;
-  
-    Projects.get(project_id).then(action => {
-      if (!action) {
-        null;
-      } else {
-        let newAction = {
-          description,
-          notes,
-          project_id,
-        };
-        Actions.insert(newAction).then(action => {
-          res.status(201).json({ success: action }); 
-        }); 
-      } 
-    }); 
-  });
-
-// middleware
-
-const validateProjectId = (req, res, next) => {
-	const { id } = req.params;
-	Projects.get(id)
-		.then((project) => {
-			project ? req.project : res.status(404).json({ message: 'Project Does Not Exist!' });
+	Projects.get()
+		.then((projects) => {
+			res.status(200).json(projects);
 		})
-		.catch((err) => {
-			res.status(500).json({ error: 'Sorry, try again!', err });
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'Error retrieving the projects' });
 		});
-	next();
-};
+});
 
-const validateProject = (req, res, next) => {
-	const { name, description } = req.body;
-	Object.entries(req.body).length === 0
-		? res.status(404).json({ message: 'No Project Data' })
-		: !name || !description
-			? res.status(400).json({ error: 'Missing required info. Please add a Name & Description!' })
-			: next();
-};
+router.get('/:id', validateId, (req, res) => {
+	Projects.get(req.projects)
+		.then((projects) => {
+			res.status(200).json(projects);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'Error retrieving the projects' });
+		});
+});
+
+router.get('/:id/actions', validateId, (req, res) => {
+	Projects.getProjectActions(req.params.id)
+		.then((action) => {
+			if (action) {
+				res.status(200).json(action);
+			} else {
+				res.status(404).json({ message: 'The project with the specified ID does not exist.' });
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'The acion information could not be retrieved.' });
+		});
+});
+
+router.post('/:id/actions', validateId, validateActions,  (req, res) => {
+    Projects.insert(req.body)
+    .then((project) => {
+        res.status(201).json(project);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error while saving the actions to the database' });
+    });
+});
+
+router.post('/', validateProjects, (req, res) => {
+	Projects.insert(req.body)
+		.then((project) => {
+			res.status(201).json(project);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'There was an error while saving the project to the database' });
+		});
+});
+
+router.put('/:id', validateId, validateProjects, (req, res) => {
+	Projects.update(req.params.id, req.body)
+		.then((project) => {
+				res.status(200).json(project);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'The project information could not be modified.' });
+		});
+});
+
+router.delete('/:id', validateId, (req, res) => {
+	Projects.remove(req.params.id)
+		.then((count) => {
+			if (count > 0) {
+				res.status(200).json({ message: 'The project has been deleted' });
+			} else {
+				res.status(404).json({ message: 'The project with the specified ID does not exist.' });
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ message: 'Error removing the project' });
+		});
+});
+
+//custom middleware
+
+function validateId(req, res, next) {
+	Projects.get(req.params.id).then((projects) => {
+		if (!projects) {
+			res.status(404).json({ message: 'projects id is missing' });
+		} else {
+			req.projects = req.params.id;
+			next();
+		}
+	});
+}
+
+function validateProjects(req, res, next) {
+	if (Object.keys(req.body).length === 0) {
+		res.status(400).json({ message: 'projects data is missing' });
+	} else if (!req.body.name || !req.body.description) {
+		res.status(400).json({ message: 'name and description are required' });
+	} else {
+		return next();
+	}
+}
+
+function validateActions(req, res, next) {
+	if (Object.keys(req.body).length === 0) {
+		res.status(400).json({ message: 'actions data is missing' });
+	} else if (!req.body.notes || !req.body.description) {
+		res.status(400).json({ message: 'notes and description are required' });
+	} else {
+		return next();
+	}
+}
 
 module.exports = router;
